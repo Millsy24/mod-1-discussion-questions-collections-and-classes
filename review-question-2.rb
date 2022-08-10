1349adff5c1e48f37c4b73238e461504bd6e5a73
Lukas Mills <lukascodes64@gmail.com>
2022-08-10 10:51:18 -0400

    we did it

--- review-question-2.rb
+++ review-question-2.rb
@@ -1,14 +1,36 @@
 # Finish the implementation of the Car class so it has the functionality described below
-
+require 'pry'
 class Car
+    attr_accessor :make, :model
+    @@all = []
+    def initialize(make:, model:)
+        @make = make
+        @model = model
+        @@all << self
+        
+    end
+    def self.drive
+        "VROOOOOOOOOOOOM!"
+    end
+    def self.all
+        @@all
+    end
+    
+    
+
 
 
 
 end
 
-volvo_lightning = Car.new("Volvo", "Lightning")
-yugo = Car.new("Zastava", "Yugo")
-lada = Car.new("AvtoVAZ", "Lada")
+
+
+
+volvo_lightning = Car.new(make:"Volvo", model:"Lightning")
+yugo = Car.new(make:"Zastava",model:"Yugo")
+lada = Car.new(make:"AvtoVAZ", model:"Lada")
+binding.pry
+
 
 volvo_lightning.make
 #=> "Volvo"
@@ -21,7 +43,7 @@ Car.drive
 Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+#BONUS:
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
--- review-question-3.rb
+++ review-question-3.rb
@@ -1,5 +1,64 @@
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
+require 'pry'
+class User
+    attr_accessor :name
+    def initialize(name)
+        @name = name
+        
+    end
+    def photos
+        Photo.all.filter do |p|
+            p.user.name == @name
+        end
+    end
+    
+
+
+end
+
+
+
+class Photo 
+    attr_accessor :user
+    attr_reader :comments
+    @@all = []
+    def initialize
+        @user = nil
+        @@all << self
+        @comments = []
+    end
+    def self.all
+        @@all
+
+    end
+    def make_comment(comment)
+        @comments << Comment.new(comment) 
+
+        
+
+    end
+    
+
+
+end
+
+
+
+
+class Comment
+    @@all = []
+    def initialize(comment)
+        @@all << self
+
+    end
+    def self.all
+        @@all
+    end
+
+
+end
+binding.pry
 
 
 sandwich_photo = Photo.new
